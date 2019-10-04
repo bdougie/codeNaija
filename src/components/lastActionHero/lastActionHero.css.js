2a85dd86492a070dab1be48120855dc7b241f595
Akinwale Habib <akinwalehabib@hotmail.com>
2019-10-04 11:56:16 +0100

    fix footer

--- src/components/lastActionHero/lastActionHero.css.js
+++ src/components/lastActionHero/lastActionHero.css.js
@@ -37,9 +37,51 @@ export const Container = styled.div`
   ${MEDIA.TABLET`
     display: flex;
     flex-direction: column-reverse;
+    align-items: center;
+    padding: 0;
+    max-width: 100%;
+    width: 100%
+    margin: 0;
+    text-align: center;
 
     img {
       margin: 16px 0;
+    }
+
+    div:last-child {
+      width: 100%;
+
+      small {
+        display: block;
+        height: 15px;	
+        color: #362B54;	
+        font-size: 12.8px;	
+        font-weight: bold;	
+        line-height: 15px;
+        margin-bottom: 6px;
+      }
+
+      h2{
+        height: 72px;
+        width: 60%;
+        margin: 0 auto;
+        font-size: 30px;
+        font-weight: bold;
+        line-height: 36px;
+        text-align: center;
+        margin-bottom: 6px;
+      }
+
+      p {	
+        height: 15px;	
+        font-size: 12.8px;	
+        line-height: 15px;
+      }
+
+      a {
+        display: block;
+        margin-bottom: 101px;
+      }
     }
   `};
 `;
