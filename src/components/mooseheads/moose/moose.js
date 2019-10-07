e5022242694eb28f787aef0a63560eaaca466d7e
Akinwale Habib <akinwalehabib@hotmail.com>
2019-10-07 15:22:27 +0100

    add max size to microsoft logo

--- src/components/mooseheads/moose/moose.js
+++ src/components/mooseheads/moose/moose.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Moose = ({ title, copy, image }) => (
   <figure>
     {image.childImageSharp.fluid.src.includes('microsoft') ?
-      <img src={image.childImageSharp.fluid.src} alt={title}/> :
+      <img style={{maxHeight: 80}} src={image.childImageSharp.fluid.src} alt={title}/> :
       <img style={{maxHeight: 40}} src={image.childImageSharp.fluid.src} alt={title}/>
     }
   </figure>
